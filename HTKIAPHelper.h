483c4a74bf5e5b33b4db47f5fd0cb4ee594e7952
Triệu Khang <hoangtrieukhang@gmail.com>
2013-10-22 14:59:15 +0700

    Add HTKIAPHelper

---
+++ HTKIAPHelper.h
@@ -0,0 +1,13 @@
+//
+//  HTKIAPHelper.h
+//  In-App-Purchase
+//
+//  Created by Triệu Khang on 22/10/13.
+//  Copyright (c) 2013 Triệu Khang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HTKIAPHelper : NSObject
+
+@end
---
+++ HTKIAPHelper.m
@@ -0,0 +1,13 @@
+//
+//  HTKIAPHelper.m
+//  In-App-Purchase
+//
+//  Created by Triệu Khang on 22/10/13.
+//  Copyright (c) 2013 Triệu Khang. All rights reserved.
+//
+
+#import "HTKIAPHelper.h"
+
+@implementation HTKIAPHelper
+
+@end
